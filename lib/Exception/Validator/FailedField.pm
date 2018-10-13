8ed60ceb6990ac91bfd42049ef88a1fdfb1560a0
Igor Sverdlov <igor-sverdlov@mail.ru>
2018-10-13 19:54:56 +0300

    redefine new for exception

--- lib/Exception/Validator/FailedField.pm
+++ lib/Exception/Validator/FailedField.pm
@@ -6,6 +6,25 @@ sub import {
     FF->export_to_level(1);
 }
 
+sub new {
+    my ($this, $text, %data) = @_;
+
+    my $class = ref($this) || $this;
+
+    $text = '' if !defined $text;
+
+    my $self   = {
+        %data,
+        (
+            blessed($text) && $text->isa('Exception')
+            ? (text => $text->{'text'}, parent => $text)
+            : (text => $text)
+        ),
+    };
+
+    return bless $self, $class;
+}
+
 package FF;
 
 use base qw(Exception::Validator::FailedField Exporter);
